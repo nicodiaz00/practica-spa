1c8063ecad12da012bf67f870e6e3f0617f49804
nicodiaz <ngdiaz00@gmailcom>
2025-05-14 12:12:52 -0300

    se crea seccion main

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 const root= document.getElementById("root");
-
-
 root.classList.add("root");
+
 header()
+main()
+
 function header(){
     const header = document.createElement("header");
     const divLeft = document.createElement("div");
     const divRight = document.createElement("div");
     const nav = document.createElement("nav");
     const unorderedList = document.createElement("ul");
+    const divImg = document.createElement("div");
     
-    header.classList.add("header");
-    divLeft.classList.add("div-header")
-    divRight.classList.add("div-header")
-    unorderedList.classList.add("ul-header");
+    const img = document.createElement("img");
+    img.src="images/oak-logo.png"
+    img.classList.add("img");
 
-    
+    header.classList.add("header");
+    divLeft.classList.add("div-r-header")
+    divRight.classList.add("div-l-header")
+    unorderedList.classList.add("ul-header");
+    divImg.classList.add("div-img")
+
+    divImg.appendChild(img);
 
     const aElement= createlink("Inicio","#inicio");
     const aElement2 = createlink("Nosotros","#nosotros");
@@ -50,13 +57,20 @@ function header(){
     header.appendChild(divRight);
     
     divRight.appendChild(nav);
+    divLeft.appendChild(divImg)
     
-    root.appendChild(header);
-    
-    
-    
-
+    root.appendChild(header);           
 }
+
+function main(){
+    const divMain = document.createElement("div")
+    divMain.classList.add("main")
+    divMain.id="inicio";
+    root.appendChild(divMain)
+}
+
+
+
 
 function createListItem(element){
     const li = document.createElement("li")
